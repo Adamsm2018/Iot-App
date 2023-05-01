51ebb378ede0891c790fe98acbd5031fe4c3bd97
Adamsm2018 <92271018+Adamsm2018@users.noreply.github.com>
2023-05-01 19:58:47 -0400

    Final Comments

--- example_resource.py
+++ example_resource.py
@@ -1,23 +1,29 @@
+# Mike Adams
+# IoT App Layer Project
+# 'example_resource.py'
+# Class referenced by main.py for server functions
+# Coapthon3 library used. Documentation and examples referenced. https://github.com/Tanganelli/CoAPthon
+
 from coapthon.resources.resource import Resource
 
-class BasicResource(Resource):
+class BasicResource(Resource):                  # Class to represent topic
     def __init__(self, name="Temperature", coap_server=None):
         super(BasicResource, self).__init__(name, coap_server, visible=True,
                                             observable=True, allow_children=True)
-        self.payload = "25c"
+        self.payload = "25c"        # Readout set to 25c
 
-    def render_GET(self, request):
+    def render_GET(self, request):      
         return self
 
-    def render_PUT(self, request):
+    def render_PUT(self, request):  # From example, unused
         self.payload = request.payload
         return self
 
-    def render_POST(self, request):
+    def render_POST(self, request): # From example, unused
         res = BasicResource()
         res.location_query = request.uri_query
         res.payload = request.payload
         return res
 
-    def render_DELETE(self, request):
+    def render_DELETE(self, request):   # From example, unused
         return True
