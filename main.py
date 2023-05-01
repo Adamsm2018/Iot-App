512204987be09fae90bc064bd7cba97d61cb7ad5
Adamsm2018 <92271018+Adamsm2018@users.noreply.github.com>
2023-05-01 19:41:59 -0400

    main.py final

--- main.py
+++ main.py
@@ -4,16 +4,14 @@ from example_resource import BasicResource
 class CoAPServer(CoAP):
     def __init__(self, host, port):
         CoAP.__init__(self, (host, port))
-        self.add_resource('basic/', BasicResource())
+        self.add_resource('Temperature/', BasicResource())
 
 def main():
     server = CoAPServer("192.168.1.217", 5683)
-    try:
-        server.listen(10)
-    except KeyboardInterrupt:
-        print("Server Shutdown")
-        server.close()
-        print("Exiting...")
+    server.listen(10)
+    print("Server Shutdown")
+    server.close()
+    print("Exiting...")
 
 if __name__ == '__main__':
     main()
