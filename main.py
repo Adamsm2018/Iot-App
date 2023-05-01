e3f78b239064f992f9b56de0f883f0bf98d8ff6e
Adamsm2018 <92271018+Adamsm2018@users.noreply.github.com>
2023-05-01 19:50:12 -0400

    Final Comments

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+# Mike Adams
+# IoT App Layer Project
+# CoAP Server
+# 'main.py'
+# Using Coapthon3 library. Documentation and examples of library referenced. https://github.com/Tanganelli/CoAPthon
+
 from coapthon.server.coap import CoAP
 from example_resource import BasicResource
 
 class CoAPServer(CoAP):
     def __init__(self, host, port):
         CoAP.__init__(self, (host, port))
-        self.add_resource('Temperature/', BasicResource())
+        self.add_resource('Temperature/', BasicResource())      # Indicates server is ACKing "Temperature" messages
 
 def main():
-    server = CoAPServer("192.168.1.217", 5683)
+    server = CoAPServer("192.168.1.217", 5683)          # IP of own machine, not the client. Listening port
     server.listen(10)
     print("Server Shutdown")
     server.close()
