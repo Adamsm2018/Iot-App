981fe8c5d15da562aa59bd264c7c4bdc18ef2738
Adamsm2018 <92271018+Adamsm2018@users.noreply.github.com>
2023-05-01 19:21:44 -0400

    COAP Client Final

--- client.py
+++ client.py
@@ -1,10 +1,29 @@
+import random
+import time
 from coapthon.client.helperclient import HelperClient
 
 host = "192.168.1.217"
 port = 5683
-path ="basic"
+path = "Temperature"
 
 client = HelperClient(server=(host, port))
-response = client.get(path)
-print(response.pretty_print())
+
+percentageLoss = 10
+messagesToSend = 100
+
+rand = random.randrange(1,100,1)
+
+startTime = time.time()
+for x in range(messagesToSend):
+    if rand > percentageLoss:
+        response = client.get_non(path)
+        print(response.pretty_print())
+        print("\n")
+    else:
+        delay(0.0045)   # To mimic lost packet, delay by average time to deliver and respond to one packet
+stopTime = time.time()
+
+timeToDelay = stopTime - startTime
+print(timeToDelay)
+
 client.stop()
