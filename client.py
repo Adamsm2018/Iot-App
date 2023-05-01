17f4adacd4905f6aa983e76c4c186775ef10170b
Adamsm2018 <92271018+Adamsm2018@users.noreply.github.com>
2023-05-01 19:55:22 -0400

    Final Comments

--- client.py
+++ client.py
@@ -1,29 +1,34 @@
+# Mike Adams
+# IoT App Layer Project
+# 'client.py'
+# COAP Client. Coapthon3 library used. Documentation and examples referenced https://github.com/Tanganelli/CoAPthon. 
+
 import random
 import time
 from coapthon.client.helperclient import HelperClient
 
-host = "192.168.1.217"
+host = "192.168.1.217"      # Addr of server
 port = 5683
-path = "Temperature"
+path = "Temperature"        # Topic
 
 client = HelperClient(server=(host, port))
 
-percentageLoss = 10
-messagesToSend = 100
+percentageLoss = 10         # Percentage packet loss
+messagesToSend = 100        
 
-rand = random.randrange(1,100,1)
+rand = random.randrange(1,100,1)    
 
-startTime = time.time()
-for x in range(messagesToSend):
+startTime = time.time()         
+for x in range(messagesToSend):                 # Sends 100 messages with a percent chance the packet is not sent
     if rand > percentageLoss:
-        response = client.get_non(path)
+        response = client.get_non(path)         #get_non() function for non-confirmed transport. get() used for confirmed
         print(response.pretty_print())
         print("\n")
     else:
         delay(0.0045)   # To mimic lost packet, delay by average time to deliver and respond to one packet
 stopTime = time.time()
 
-timeToDelay = stopTime - startTime
+timeToDelay = stopTime - startTime      
 print(timeToDelay)
 
 client.stop()
