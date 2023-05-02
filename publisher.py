b75cedd834637846fe3fbb38e43b05a801a2da76
Adamsm2018 <92271018+Adamsm2018@users.noreply.github.com>
2023-05-01 20:18:31 -0400

    Final Comments

--- publisher.py
+++ publisher.py
@@ -1,6 +1,7 @@
 # Mike Adams
 # Iot Project
 # 'client.py'
+# Paho mqtt library used. See documentation https://pypi.org/project/paho-mqtt/
 
 import time
 import paho.mqtt.client as mqtt
@@ -9,7 +10,7 @@ def on_connect(client, userdata, flags, rc):
    if rc==0:
       print("connected ok")
 
-broker_address="192.168.1.217"
+broker_address="192.168.1.217"         # IP of broker. Same machine as this script
 # broker_address="iot.eclipse.org" #use external broker
 client = mqtt.Client("P1")                  #create new instance
 #client.on_connect=onconnect  #bind call back function
@@ -18,15 +19,15 @@ startTime = time.time()
 client.loop_start()  #Start loop
 
 
-packetsToSend = 100
-currPacket = "Packet #"
+packetsToSend = 100        # Sends 100 packets
+currPacket = "Packet #"    
 
 for x in range(packetsToSend):
-   currPacket = "Packet " + str(x)
+   currPacket = "Packet " + str(x)     # Numbers packets so which packet is recieved can be tracked
    client.publish("Packet", currPacket, 2, 0)    #publish
-   if client.getSendAgain() == 1:
+   if client.getSendAgain() == 1:      # If resend flag, from corruption functions
       print("Sending packet again")
-      client.publish("Packet", currPacket, 0, 0)    #publish
+      client.publish("Packet", currPacket, 0, 0)    #Resends once if packet was corrupted on first try
    client.setSendAgain(0)
 
 client.loop_stop()    #Stop loop
